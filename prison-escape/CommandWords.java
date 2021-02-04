9a7f1e9312aafa06988e8894c937d5c0ccd37d01
thomtreebus <73241617+thomtreebus@users.noreply.github.com>
2021-02-04 08:37:05 +0100

    Add files via upload

---
+++ prison-escape/CommandWords.java
@@ -0,0 +1,52 @@
+/**
+ * This class is part of the "Prison Escape" application. 
+ * "Prison Escape" is a simple, text based game.
+ * 
+ * This class holds an enumeration of all command words known to the game.
+ * It is used to recognise commands as they are typed in.
+ *
+ * @author  Michael Kölling and David J. Barnes
+ * Completed by Thom Treebus
+ * @version 2020.12.02
+ */
+
+public class CommandWords
+{
+    // a constant array that holds all valid command words
+    private static final String[] validCommands = {
+        "help", "quit", "go", "back", "take", "drop", "inspect", "inventory", "drink", "give", "interact"
+    };
+
+    /**
+     * Constructor - initialise the command words.
+     */
+    public CommandWords()
+    {
+        // nothing to do at the moment...
+    }
+
+    /**
+     * Check whether a given String is a valid command word. 
+     * @return true if it is, false if it isn't.
+     */
+    public boolean isCommand(String aString)
+    {
+        for(int i = 0; i < validCommands.length; i++) {
+            if(validCommands[i].equals(aString))
+                return true;
+        }
+        // if we get here, the string was not found in the commands
+        return false;
+    }
+
+    /**
+     * Print all valid commands to System.out.
+     */
+    public void showAll() 
+    {
+        for(String command: validCommands) {
+            System.out.print(command + "  ");
+        }
+        System.out.println();
+    }
+}
